02483076f75a30129451ab83d7a051a13a62780c
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2022-07-19 13:32:41 +0900

    수정

--- bbibbo/src/Introduction.js
+++ bbibbo/src/Introduction.js
@@ -5,15 +5,15 @@ import "./App.css";
 
 function Introduction(){
     return(
-        <div class>
-            <div class="head">몽글몽글 소개</div>
+        <div>
+            <div className="head">몽글몽글 소개</div>
 
             <div>
                 <div>
-                    <p class="btn">✔</p>
-                    <p class="locate1">몽글몽글이란?</p>
+                    <p className="btn">✔</p>
+                    <p className="locate1">몽글몽글이란?</p>
                 </div>
-                <div class="locate1-2">
+                <div className="locate1-2">
                     <p>몽글몽글은 대소고 학생들이 자유롭게 이용할 수 있는 커뮤니티 사이트입니다.
                     또한 몽글몽글은 익명과 실명을 쓸 수 있어서 개인정보를 지킬 수 있습니다.</p>
                 </div>
@@ -21,20 +21,20 @@ function Introduction(){
 
             <div>
                 <div>
-                    <p class="btn">✔</p>
-                    <p class="locate1">주의사항</p>
+                    <p className="btn">✔</p>
+                    <p className="locate1">주의사항</p>
                 </div>
-                <div class="locate1-2">
+                <div className="locate1-2">
                     <p>다른 사용자가 쓴 글을 허락없이 퍼가거나 부적절한 행동시 관련법률 및 운영정책에 의해 제재 받을 수 있습니다. </p>
                 </div>
             </div>
 
             <div>
                 <div>
-                    <p class="btn">✔</p>
-                    <p class="locate1">몽글몽글 청원작성</p>
+                    <p className="btn">✔</p>
+                    <p className="locate1">몽글몽글 청원작성</p>
                 </div>
-                <div class="locate1-2">
+                <div className="locate1-2">
                     <p>현재 페이지 상단에 있는 "청원하기"를 클릭하시면 작성하실 수 있고
                         청원글은 청원을 하시면 작성하신 글들을 볼 수 있습니다.
                     </p>
